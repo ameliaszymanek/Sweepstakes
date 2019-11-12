b6b9bcab4cfbc0c4b4dc2a4c66d742146ab17df2
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-12 09:59:17 -0600

    created 3 methods in UserInterface- GetFirstName, GetLastName, GetEmailAddress.

---
+++ Sweepstakes/UserInterface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    static class UserInterface
+    {
+        //MM
+
+
+        //CTOR
+
+
+        //MV
+        public static string GetFirstName()
+        {
+            Console.WriteLine("What is your first name?");
+            Console.ReadLine();
+            return Console.ReadLine();
+        }
+
+        public static string GetLastName()
+        {
+            Console.WriteLine("What is your last name?");
+            Console.ReadLine();
+            return Console.ReadLine();
+        }
+
+        public static string GetEmailAddress()
+        {
+            Console.WriteLine("What is your email address?");
+            Console.ReadLine();
+            return Console.ReadLine();
+        }
+
+        //assign registration number???
+        //public static int AssignRegNum()
+        //{
+        //    //logic
+        //}
+    }
+}
