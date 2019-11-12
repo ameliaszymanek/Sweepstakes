ccfed11e9235b2db5eeffa68e81cc502c9b9ad42
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-12 08:19:51 -0600

    Init commit. Contestant class made.

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        //first name, last name, email address, registration number
+
+        //MM
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        private int registrationNumber;
+
+        //CTOR
+        public Contestant()
+        {
+            Contestant newContestant = new Contestant();
+        }
+
+        //MV
+        public void AssignValues()
+        {
+            firstName = UserInterface.GetFirstName;
+            lastName = UserInterface.GetLastName;
+            emailAddress = UserInterface.GetEmail;
+            registrationNumber = UserInterface.GetRegNumber;
+
+            //NOW CREATE USERINTERFACE STATIC CLASS
+        }
+    }
+}
